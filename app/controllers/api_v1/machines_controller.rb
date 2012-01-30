04c4123f1e88d06b91ba6198c7724822ac6feb87
Simon Menke <simon.menke@gmail.com>
2012-01-31 00:40:41 +0100

    Improved the tasks API call

--- app/controllers/api_v1/machines_controller.rb
+++ app/controllers/api_v1/machines_controller.rb
@@ -240,7 +240,12 @@ class ApiV1::MachinesController < ApplicationController
       return
     end
 
-    render :json => machine.pluto_process_instances
+    applications = Core::Application.all
+    releases     = Core::Release.where(id: applications.map(&:active_core_release_id)).all
+    definitions  = Pluto::ProcessDefinition.where(core_release_id: releases.map(&:id)).all
+    instances    = Pluto::ProcessInstance.where(pluto_process_definition_id: definitions.map(&:id)).all
+
+    render :json => instances
   end
 
 end
