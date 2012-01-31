36a93a35c0715a034d8f4c785947a10ac2b619f1
Simon Menke <simon.menke@gmail.com>
2012-01-31 15:08:48 +0100

    Packaging as a npm package

---
+++ script/install.sh
@@ -0,0 +1,14 @@
+export rvm_project_rvmrc=0
+export rvm_pretty_print=0
+[[ -f /usr/local/rvm/scripts/rvm ]] && RVM_BOOT=/usr/local/rvm/scripts/rvm
+[[ -f $HOME/.rvm/scripts/rvm     ]] && RVM_BOOT=$HOME/.rvm/scripts/rvm
+[[ "x" != "x$RVM_BOOT"           ]] && . $RVM_BOOT
+
+. ./.rvmrc
+
+bundle install \
+  --gemfile Gemfile \
+  --path    vendor/bundle
+  --deployment
+  --quiet
+  --without development test
