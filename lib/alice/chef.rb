14b7a3549164a4f8170a57f260f40b72f1572499
Simon Menke <simon.menke@gmail.com>
2012-02-27 13:20:45 +0100

    Fixed a typo

--- lib/alice/chef.rb
+++ lib/alice/chef.rb
@@ -15,7 +15,7 @@ module Alice::Chef
     def update
       application_key = Digest::SHA1.hexdigest(@release.core_application.name)
 
-      definitions   = Pluto::ProcessDefinition.where(owner_type: 'Core::Release', owner_id: @releases.id).all
+      definitions   = Pluto::ProcessDefinition.where(owner_type: 'Core::Release', owner_id: @release.id).all
       new_processes = Pluto::ProcessInstance.where(pluto_process_definition_id: definitions.map(&:id)).all
       new_processes = new_processes.index_by { |p| Digest::SHA1.hexdigest([application_key, ui_name].join('')) }
 
