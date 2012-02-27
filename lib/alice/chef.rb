aa82504008d6ad88bf9725a45b9caefb934c8077
Simon Menke <simon.menke@gmail.com>
2012-02-27 13:41:38 +0100

    More fixes for chef-server

--- lib/alice/chef.rb
+++ lib/alice/chef.rb
@@ -17,10 +17,10 @@ module Alice::Chef
 
       definitions   = Pluto::ProcessDefinition.where(owner_type: 'Core::Release', owner_id: @release.id).all
       new_processes = Pluto::ProcessInstance.where(pluto_process_definition_id: definitions.map(&:id)).all
-      new_processes = new_processes.index_by { |p| Digest::SHA1.hexdigest([application_key, ui_name].join('')) }
+      new_processes = new_processes.index_by { |p| Digest::SHA1.hexdigest([application_key, p.ui_name].join('')) }
 
       query = C::Search::Query.new
-      query.search('alice-processes', "application_key:#{application_key}").each do |process|
+      query.search('alice-processes', "application_key:#{application_key}") do |process|
         new_process = new_processes.delete(process['id'])
 
         if new_process
