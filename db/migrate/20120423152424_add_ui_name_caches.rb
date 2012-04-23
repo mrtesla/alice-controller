7a78c019cfca919896027ec67ef47c3a41f773aa
Simon Menke <simon.menke@gmail.com>
2012-04-23 17:38:00 +0200

    Fix migration

--- db/migrate/20120423152424_add_ui_name_caches.rb
+++ db/migrate/20120423152424_add_ui_name_caches.rb
@@ -9,14 +9,6 @@ class AddUiNameCaches < ActiveRecord::Migration
     change_table :http_backends do |t|
       t.string :ui_name
     end
-
-    ::Http::Router.reset_column_information
-    ::Http::Passer.reset_column_information
-    ::Http::Backend.reset_column_information
-
-    ::Http::Router.all.map(&:save)
-    ::Http::Passer.all.map(&:save)
-    ::Http::Backend.all.map(&:save)
   end
 
   def down
